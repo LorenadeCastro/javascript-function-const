b72889c88c401f1c4e464765bf2e7168ab82a14b
Lorena de Castro <139800465+LorenadeCastro@users.noreply.github.com>
2023-08-08 14:36:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const carro = {marca: "fiat",
+modelo: "uno",
+ano: "1989",
+cor: "prata",
+placa: "rpq895",
+portas: "quatro",
+motor: function(){alert('Galooooooooooo')}
+};
+
+console.log (carro.motor())
